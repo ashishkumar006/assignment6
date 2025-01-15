40a4b72be668e1f7478e8206dafca1b69ed8d65f
Ashish <ashishkumarreddythodiam@gmail.com>
2025-01-15 21:05:20 +0530

    fixed errors

--- check_model.py
+++ check_model.py
@@ -1,6 +1,6 @@
+from __future__ import print_function
 import torch
 import torch.nn as nn
-from __future__ import print_function
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
